2e9569d722b1550d3f0d0cb507efa1cf8748c97e
Muhammed Sanusi <muhammedsanusi890@gmail.com>
2026-03-04 09:39:46 -0500

    completed CRUD functionsand routes for employees

--- Backend/Routes/Employee.py
+++ Backend/Routes/Employee.py
@@ -1,18 +1,36 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from Backend.Utility.dependencies import get_db, hr_only
-from Backend.Models.Employee import Employee, User
-from Backend.Schemas.Employee import EmployeeCreate, EmployeeOut
-from Backend.Services.employee_service import create_employee
+from Backend.Models.User import User
+from Backend.Schemas.Employee import EmployeeCreate, EmployeeOut, EmployeeUpdate
+from Backend.Services.employee_service import create_employee, update_employee, show_employee, show_all_employees, delete_employee
 
 router = APIRouter()
 
+@router.get('/all')
+async def get_all(user : User = Depends(hr_only), db : Session = Depends(get_db)):
+   all_employees = show_all_employees(db)
+   return all_employees
+
+
+@router.get('/{employee_id}')
+async def get_employee(employee_id : int, user : User = Depends(hr_only), db : Session = Depends(get_db)):
+   employee_details = show_employee(employee_id, db)
+   return employee_details
 
 @router.post('/sign_up', response_model = EmployeeOut)
-async def register(employee:EmployeeCreate,  user : User = Depends(hr_only), db : Session = Depends(get_db)) :
+async def register(employee:EmployeeOut,  user : User = Depends(hr_only), db : Session = Depends(get_db)) :
    new_employee =  create_employee(employee, db)
    return new_employee
 
 
+@router.patch('/{employee_id}/update')
+async def update_info(employee_id : int , employee: EmployeeUpdate, user : User = Depends(hr_only), db : Session = Depends(get_db)):
+   employee_change = update_employee(employee_id, employee, db)
+   return employee_change
 
-    
+@router.delete('{employee_id}/delete')
+async def remove_employee(employee_id : int , user : User = Depends(hr_only), db : Session = Depends(get_db)):
+   employee = delete_employee(employee_id, db)
+   return employee
+
--- Backend/Schemas/Employee.py
+++ Backend/Schemas/Employee.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from datetime import date, datetime
+from datetime import date
 from typing import Optional
 
 
@@ -13,7 +13,38 @@ class EmployeeCreate(BaseModel):
     city: Optional[str] = None
     state: Optional[str] = None
     zip_code: Optional[str] = None
-    date_of_birth: Optional[date] = None
-    hire_date: date
-    ssn: Optional[str] = None
-    employment_status: str = "active"
+    date_of_birth: Optional[date] = None      
+    hire_date: date                           
+    ssn: str                                  
+    employment_status: Optional[str] = "active"
+
+
+class EmployeeOut(BaseModel):
+    employee_id: int
+    first_name: str
+    last_name: str
+    email: str
+    phone: Optional[str]
+    address: Optional[str]
+    city: Optional[str]
+    state: Optional[str]
+    zip_code: Optional[str]   
+
+    class Config:
+        orm_mode = True
+
+
+
+class EmployeeUpdate(BaseModel):
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    email: Optional[str] = None
+    phone: Optional[str] = None
+    address: Optional[str] = None
+    city: Optional[str] = None
+    state: Optional[str] = None
+    zip_code: Optional[str] = None
+
+    class Config:
+        orm_mode = True
+
